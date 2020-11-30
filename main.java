28e14c1bf8cd6afe7eb0c5b77534979d0115ad52
Nihar Patel <58643536+niharp73@users.noreply.github.com>
2020-11-30 14:18:37 -0500

    Update main.java

--- main.java
+++ main.java
@@ -224,7 +224,7 @@ public class HospitalManagement
         //System.out.println("--------------------------------------------------------------------------------");
         int count1 = 4, count2 = 4, count3 = 4, count4 = 4, count5 = 4, count6 = 4;
         System.out.println("\n--------------------------------------------------------------------------------");
-        System.out.println("            *** Welcome to Hospital Management Project ***");
+        System.out.println("            *** Welcome to Covid-19 and Management Project ***");
         System.out.println("--------------------------------------------------------------------------------");
         System.out.print("Date: " + months[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.DATE) + " " + calendar.get(Calendar.YEAR));
         System.out.println("\t\t\t\t\t\tTime: " + calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND));
